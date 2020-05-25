53d5a3e795092cd732b5659c88fada33bd8ede38
Nicolas Lopes Aquino <nicolaslopesbusiness@gmail.com>
2020-05-25 13:22:06 -0300

    Configuring the linter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
 
 function App() {
-  return <div className="App"></div>;
+    const [qtdSites, setQtdSites] = useState([]);
+
+    useEffect(async () => {
+        const response = api.get('/getQtd');
+        setQtdSites(response.data.qtdSites);
+    }, []);
+
+    return (
+        <div className="App">
+            <span>{qtdSites}</span>
+        </div>
+    );
 }
 
 export default App;
